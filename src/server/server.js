5c911f6a7d53a383b1ce6f77cfde05d003dbe6ec
Param Jain <param_jain@icloud.com>
2020-05-19 20:33:05 +0530

    Deploying together trial attempt

--- src/server/server.js
+++ src/server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
     res.send("App works!!");
   });
 
